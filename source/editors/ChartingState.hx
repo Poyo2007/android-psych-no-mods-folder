2be2c52016a34e1c82ffaa590e02549ccc01fd28
* <itssng7@gmail.com>
2022-02-26 02:52:00 -0500

    wow you can save charts

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -2636,11 +2636,7 @@ class ChartingState extends MusicBeatState
 
 		if ((data != null) && (data.length > 0))
 		{
-			_file = new FileReference();
-			_file.addEventListener(Event.COMPLETE, onSaveComplete);
-			_file.addEventListener(Event.CANCEL, onSaveCancel);
-			_file.addEventListener(IOErrorEvent.IO_ERROR, onSaveError);
-			_file.save(data.trim(), Paths.formatToSongPath(_song.song) + ".json");
+			openfl.system.System.setClipboard(data.trim());
 		}
 	}
 	
